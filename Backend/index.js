a92909ad3a1c0bef3f12746865bd6e474f9636ab
Oscar Cruz <oscarcruzsena2006@gmail.com>
2025-11-27 12:00:20 -0500

    Add files via upload

---
+++ Backend/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+const PUERTO = 3000;
+
+// Middlewares
+app.use(cors());
+app.use(bodyParser.json());
+
+// Rutas
+app.use("/api/edumultipro", require("./routes/usuarios"));
+
+// Ruta principal
+app.get("/", (req, res) => {
+    res.send("Hola desde el servidor");
+});
+
+app.listen(PUERTO, () => {
+    console.log(`🚀 Servidor escuchando en http://localhost:${PUERTO}`);
+});
+
+const path = require("path");
+app.use('/imagenes', express.static(path.join(__dirname, 'imagenes')));
+
+module.exports = app; // <- EXPORTAR para tests
